bdece759d16230884223415a064ad4aa07d4bcac
Damodar <damodarkumar25@gmail.com>
2020-09-01 13:55:28 +0530

    Second Commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblockApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblockApplication.java
@@ -1,7 +1,10 @@
 package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//
 
 @SpringBootApplication
 public class SpringbootBuildingblockApplication {
